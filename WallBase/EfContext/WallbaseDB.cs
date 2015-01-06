86a52c75a9e12a29bc66b4b3384667257e5248f0
Dmitry Vakylenko <kremdima@mail.ru>
2015-01-06 19:19:57 +0200

    removed unused files

--- WallBase/EfContext/WallbaseDB.cs
+++ WallBase/EfContext/WallbaseDB.cs
@@ -16,7 +16,8 @@ namespace EfContext
 
         }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {//    modelBuilder.Entity<Friend>().HasRequired(q => q.User).WithMany(w => w.UserFriends).HasForeignKey(q => q.UserId).WillCascadeOnDelete(false);
+        {
+           //    modelBuilder.Entity<Friend>().HasRequired(q => q.User).WithMany(w => w.UserFriends).HasForeignKey(q => q.UserId).WillCascadeOnDelete(false);
             //          modelBuilder.Entity<UserRewards>().HasRequired(q => q.User).WithMany(w => w.Rewards).HasForeignKey(q => q.UserId);
             //  modelBuilder.Entity<Friend>().HasKey(t => new { t.UserId, t.FriendId });
             //modelBuilder.Entity<User>().HasMany(m => m.RequestUsers).WithMany().Map(
